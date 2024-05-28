d7dbd5dde961baa24a5a971168e1d96a69353686
Agustin Gutierrez <agustin.gutierrez.ochoa@gmail.com>
2024-05-28 20:02:07 -0300

    Fixing keys

--- src/stock_market/constants.py
+++ src/stock_market/constants.py
@@ -1,1 +1,8 @@
+TIME_SERIES_KEY = 'Time Series (Daily)'
+INFORMATION = 'Information'
+CLOSE_KEY = '4. close'
+SYMBOL_KEY = '2. Symbol'
+OPEN_KEY = '1. open'
+HIGH_KEY = '2. high'
+LOW_KEY = '3. low'
 VANTAGE_URL = "https://www.alphavantage.co/query?function={function}&symbol={symbol}&outputsize={size}&apikey={api_key}"
