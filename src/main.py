3fa302bd5a722313690356fc7a468d696d31cc0d
Agustin Gutierrez <agustin.gutierrez.ochoa@gmail.com>
2024-05-28 17:50:25 -0300

    Implementing API throtling

--- src/main.py
+++ src/main.py
@@ -3,7 +3,16 @@ from src.stock_market.router import router as stock_market_router
 from src.auth.router import router as auth_router
 from src.logging_config import logger
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+limiter = Limiter(key_func=get_remote_address)
+
 app = FastAPI(title="Stock Market API Service")
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.include_router(stock_market_router)
 app.include_router(auth_router)
