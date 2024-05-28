dc7412bd6fbc57c5d14c8d4887764d59dc4753ba
Agustin Gutierrez <agustin.gutierrez.ochoa@gmail.com>
2024-05-28 16:50:17 -0300

    Logging every api call

---
+++ src/logging_config.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import logging
+
+log_file_path = Path('/logs/api.log')
+
+log_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "file": {
+            "class": "logging.FileHandler",
+            "formatter": "default",
+            "filename": str(log_file_path),
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["file", "console"],
+    },
+}
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger(__name__)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from src.stock_market.router import router as stock_market_router
 from src.auth.router import router as auth_router
-
+from src.logging_config import logger
 
 app = FastAPI(title="Stock Market API Service")
 
 app.include_router(stock_market_router)
 app.include_router(auth_router)
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(f"Request: {request.method} {request.url}")
+    response = await call_next(request)
+    logger.info(f"Response status: {response.status_code}")
+    return response
